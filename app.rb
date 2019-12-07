7956750cd11572f123363de4910cc17adc1f3a6a
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-12-07 12:50:38 +0000

    Added get request for a full url to the controller.

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
-require 'sinatra'
+require 'sinatra/base'
+require_relative 'lib/url_shortener'
 
 class App < Sinatra::Base
+
+  before do
+    @url_shortener = UrlShortener.new
+  end
 
   get '/' do
     'URL Shortener'
   end
 
+  get '/:fullurl' do
+    fullurl = params[:fullurl]
+
+    @url_shortener.add_url(fullurl)
+    @url_shortener.shorten_url(fullurl)
+  end
 end
