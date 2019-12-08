b9a0a3a68d21bad81eb71a641ce60f0589538af1
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-12-08 15:14:08 +0000

    Refactored post method in app to use create_new_shortener method.

--- app.rb
+++ app.rb
@@ -13,16 +13,16 @@ class App < Sinatra::Base
 
   post '/' do
     url = JSON.parse(request.body.read)
-    full_url = @urlshortener.add_url(url['url'])
+    @urlshortener.create_new_shortener(url['url'])
+    { short_url: @urlshortener.return_short_url, url: @urlshortener.return_url["url"] }.to_json
 
-    short_url = @urlshortener.shorten_url(url['url'])
-    { short_url: "#{short_url}", url: @urlshortener.return_url["url"] }.to_json
+    puts @urlshortener.urls
   end
 
   get '/:short_url' do
-    puts @urlshortener.return_url
+    short_url = params[:short_url]
 
-    redirect "#{@urlshortener.urls["url"]}", 301
+    redirect "#{@urlshortener.urls[params[short_url]]}", 301
   end
 
   run! if app_file == $0
